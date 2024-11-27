4e0b1f1d54a02a572ff4f5e27d4e550701c4dd15
DioneKx <mjhonnyvieira@gmail.com>
2024-11-27 18:30:54 -0300

    Jhonny: Poke-Api

--- pokedex-app/src/pages/Home/index.tsx
+++ pokedex-app/src/pages/Home/index.tsx
@@ -3,8 +3,37 @@ import { Text, View } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { styles } from './style';
 
+interface Poke {
+    entry_number: number;
+    pokemon_species: {
+        name: string;
+        url: string;
+    };
+};
+
 export default function HomePage() {
     const navigation = useNavigation();
+    const [pokedex, setPokedex] = React.useState([]);
+
+    React.useEffect(() => {
+        fetch(
+            'https://pokeapi.co/api/v2/pokedex/1',
+            {
+                method: 'GET',
+                mode: "cors",
+                headers: {
+                    'Content-Type': 'application/json',
+                    "Access-Control-Allow-Origin": "*"
+                }
+            }).then(
+                (res) => res.json()
+            ).then((data) => {
+                const temp: Poke[] = data.pokemon_entries;
+                setPokedex(temp);
+            }).catch(
+                (error) => console.log(error)
+            )
+    }, []);
 
     return (
         <View style={styles.container}>
